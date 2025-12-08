f8bb6251a0a6437088ad10b2cf2b01f97872fc29
Martin Fischer <115947567+mamf92@users.noreply.github.com>
2025-12-08 13:13:36 +0100

    feat: add LoginPage - Created LoginPage using LoginForm component - Created onSumbit handler for login form as callback function - Implemented error handling and redirect on successful login

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -1,13 +1,47 @@
-// import { renderLoginForm } from '../components/forms/LoginForm';
+import { renderLoginForm } from '../components/forms/LoginForm';
+import { login } from '../api/authService';
+import { setAuthState } from '../auth/authState';
+import { showModal } from '../components/ui/Modals';
+import { ApiClientError } from '../api/apiClient';
+
+const BASE = import.meta.env.BASE_URL;
 
 export function renderLoginPage() {
   const loginViewContainer = document.createElement('div');
   loginViewContainer.className =
-    'flex flex-col items-center justify-center w-full';
-  const heading = document.createElement('h1');
-  heading.className = 'font-heading text-4xl text-wexham-dark mb-6';
-  heading.textContent = 'Login page';
-  loginViewContainer.appendChild(heading);
-  // loginViewContainer.appendChild(renderLoginForm());
+    'flex flex-col items-center justify-center w-full bg-aurora-silk';
+  loginViewContainer.appendChild(renderLoginForm(handleLoginFormSubmit));
   return loginViewContainer;
+}
+
+/**
+ * Handles the login form submission
+ * @param event - The submit event
+ */
+export async function handleLoginFormSubmit(event: SubmitEvent) {
+  event.preventDefault();
+  const form = event.target as HTMLFormElement;
+  const formData = new FormData(form);
+  const email = formData.get('email') as string;
+  const password = formData.get('password') as string;
+
+  try {
+    const response = await login({ email: email, password: password });
+    if (response && response.data && response.data.accessToken) {
+      setAuthState(response.data);
+      window.location.href = BASE;
+    }
+  } catch (error) {
+    let errorMessage = 'An unknown error occurred.';
+    if (error instanceof ApiClientError) {
+      errorMessage = error.apiError.errors?.[0]?.message || error.message;
+    } else if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+    showModal({
+      title: 'Login failed.',
+      message: errorMessage,
+      icon: 'error',
+    });
+  }
 }
